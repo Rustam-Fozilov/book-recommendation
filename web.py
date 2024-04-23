5492470a3f8d2a4f5d40a5333e9aac79506a3803
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-04-23 17:37:51 +0500

    render-config

--- web.py
+++ web.py
@@ -19,5 +19,5 @@ def read_item(isbn: str):
     return knn.get_recommends(isbn)
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
+# if __name__ == '__main__':
+#     uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
