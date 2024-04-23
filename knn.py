85a07c02f1f4984a80dbaa2d0b8bf93085eb3861
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-04-23 17:43:32 +0500

    render-config2

--- knn.py
+++ knn.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 # from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 books_filename = 'books.csv'
