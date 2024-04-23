ca95555217039bd2a64ce3c9de605d0e7e613dad
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-04-23 15:36:55 +0500

    comment remove

--- web.py
+++ web.py
@@ -19,10 +19,5 @@ def read_item(isbn: str):
     return knn.get_recommends(isbn)
 
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
-
 # if __name__ == '__main__':
 #     uvicorn.run(app, host='127.0.0.1', port=8000)
